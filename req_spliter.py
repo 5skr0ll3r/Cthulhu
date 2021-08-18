708c290b571db2078da7b397af0a510d22b77e10
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2021-08-18 19:42:28 +0300

    Update req_spliter.py

--- req_spliter.py
+++ req_spliter.py
@@ -3,11 +3,11 @@ import re,codecs
 #Returns file name that GET's requested by client
 def req_get_spliter(conn):
     print("module req_spliter re_get_spliter(conn) conn type is: ", type(conn))
-    code = re.search('GET', str(conn))#str(codecs.decode(conn, 'UTF-8')))
+    code = re.search('GET', conn)#str(codecs.decode(conn, 'UTF-8')))
     req = code.group()
     if "GET" in req:
         print("Is GET request")
-        link = re.search('/.+.\.html|/.+.\.css|/.+.\.js', codecs.decode(conn, 'UTF-8'))
+        link = re.search('/.+.\.html|/.+.\.css|/.+.\.js', conn)#codecs.decode(conn, 'UTF-8'))
         name = link.group()
         file = name.split("/")
         print("File is: ", file)
@@ -15,4 +15,5 @@ def req_get_spliter(conn):
             if ".css" in i or ".js" in i or "html" in i:
                 return i
     return False
+
 
