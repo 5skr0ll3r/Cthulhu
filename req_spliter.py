31a2279e99871b61662acab32f8cc1371e212366
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2021-08-20 23:10:22 +0000

    Update req_spliter.py
    fixed attribute error, external css-js still not working

--- req_spliter.py
+++ req_spliter.py
@@ -3,16 +3,26 @@ import re,codecs
 #Returns file name that GET's requested by client
 def req_get_spliter(connection):
     print("module req_spliter re_get_spliter(connection) conn type is: ", type(connection))
-    code = re.search('GET', codecs.decode(connection, 'UTF-8'))
-    req = code.group()
-    if "GET" in req:
+    try:
+        print("Try: ", connection)
+        code = str(re.search('GET', connection).group())#codecs.decode(connection, 'UTF-8'))
+        print("code is type: ", type(code))
+    except AttributeError:
+        print("AttributeError ", connection)
+        code = str(re.search('GET', connection))
+        print("code is type: ", type(code))
+    if "GET" in code.split():
         print("Is GET request")
-        link = re.search('/.+.\.html|/.+.\.css|/.+.\.js', connection)#codecs.decode(connection, 'UTF-8'))
-        name = link.group()
-        file = name.split("/")
+        try:
+            link = re.search('/.+.\.html|/.+.\.css|/.+.\.js', connection).group()#codecs.decode(connection, 'UTF-8'))
+        except AttributeError:
+            link = re.search('/.+.\.html|/.+.\.css|/.+.\.js', connection)
+        file = link.split("/")
         print("File is: ", file)
         for i in file:
             if ".css" in i or ".js" in i or "html" in i:
                 return i
     return False
+
+
 
